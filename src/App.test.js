a0844dbce72b8c642c2bacc539a76c1c806e775b
Neeraj Jain <neeraj.jain@nagarro.com>
2019-01-29 13:40:47 +0530

    add jest test for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+Enzyme.configure({adapter: new Adapter()});
 import App from './App';
 
 /*
@@ -14,3 +16,24 @@ import App from './App';
 * Please add any other test that you think could be missing to adequately test the App
 *
 * */
+
+describe('tests for the App.js', () => {
+  
+    it('should render an input box', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('input').exists()).toBe(true);
+    });
+  
+    it('should render a button', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('button').exists()).toBe(true);
+    });
+
+    
+    // it('should verify the presence of image on clicking the button', () => {
+    //   const wrapper = shallow(<App />);
+    //   wrapper.find('button').simulate('click');
+    //   expect(wrapper.find('image').exists()).toBe(true);
+    // });
+    
+  });
