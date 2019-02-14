e87e8bf854b772dc346ec2ca7a7731ad4bc8f1f1
Angelos <aggetheod@gmail.com>
2019-02-14 18:53:11 +0100

    Adding api tests and answering questions

--- src/Api.test.js
+++ src/Api.test.js
@@ -1,5 +1,4 @@
 import ask from './Api';
-import { isMainThread } from 'worker_threads';
 
 /*
 *
@@ -16,8 +15,70 @@ import { isMainThread } from 'worker_threads';
 
 
 describe('API Caller', () => {
-    it.only('should call YesNo API', () => {
-            
-    })
-    
+    it('should call YesNo API', done  => {
+        const mockResponse = {};
+        const spy = jest.spyOn(global, 'fetch')
+                        .mockImplementation(() => Promise.resolve({
+                            json: () => (mockResponse)
+                    }));
+        const endpoint = `https://yesno.wtf/api/`
+
+        return ask().then(() => { 
+            expect(spy).toHaveBeenCalledWith(endpoint);
+
+        spy.mockClear();
+        done()
+        });
+    });
+
+    it('should call the API once', done => {
+        const mockResponse = {};
+        const spy = jest.spyOn(global, 'fetch')
+                        .mockImplementation(() => Promise.resolve({
+                            json: () => (mockResponse)
+                    }));
+
+        return ask().then(() => { 
+            expect(spy).toHaveBeenCalledTimes(1);
+
+        spy.mockClear();
+        done();
+        });
+    });
+
+    it('should return response', done => {
+        const mockResponse = {};
+        const spy = jest.spyOn(global, 'fetch')
+                        .mockImplementation(() => Promise.resolve({
+                            json: () => (mockResponse)
+                    }));
+
+        return ask().then(() => { 
+            expect(spy).toHaveReturnedTimes(1);
+
+        spy.mockClear();
+        done();
+        });
+    });
+
+    it('should return in JSON format', done => {
+        const JSONbody = {
+            answer: "no",
+            forced: false,
+            image: "https://yesno.wtf/assets/no/21-05540164de4e3229609f106e468fa8e7.gif"
+            };
+        const spy = jest.spyOn(global, 'fetch')
+                        .mockImplementation(() => Promise.resolve({
+                            json: () => (JSONbody)
+                    }));
+                    
+
+        return ask().then(response => { 
+            expect(response).toEqual(JSONbody);
+
+        spy.mockClear();
+        done();
+        });
+    });
+
 });
--- src/App.test.js
+++ src/App.test.js
@@ -36,9 +36,9 @@ describe('App tests', () => {
         
         /* 
         *  Or maybe if we wanted to type something, it should something like:
-        *  let text = "Is any god reading this?"
+        *  let userText = "Is any god reading this?"
         *  inputBox.simulate('change', {target: {value: {text}} })
-        *  expect(inputBox.text()).toEqual(text)
+        *  expect(inputBox.text()).toEqual(userText)
         */
     });
 
@@ -46,37 +46,46 @@ describe('App tests', () => {
         const component = shallow(<App  />);
         let button = component.find('button')
         let buttonText = "Ask to the Gods of the Internet!"
+
         expect(button.text()).toEqual(buttonText)
     });    
 
     it('<h1> appears when button is clicked', () => {
         const component = shallow(<App  />);
-        let button = component.find('button')
-        button.simulate('click')
+        let button = component.find('button');
 
-        let h1 = component.find('h1')
-        expect(h1).toBeTruthy()
+        button.simulate('click');
+        component.update();
+
+        let h1 = component.find('h1');
+
+        expect(h1).toBeTruthy();
     });
 
     it('<img> appears when button is clicked', () => {
         const component = shallow(<App  />);
-        let button = component.find('button')
-        button.simulate('click')
+        let button = component.find('button');
 
-        let img = component.find('img')
-        expect(img).toBeTruthy() 
+        button.simulate('click');
+        component.update();
+
+        let img = component.find('img');
+
+        expect(img).toBeTruthy(); 
     });
 
-    it('verifies the correct image loads', () => {
+    it('verifies the correct content loads', () => {
         // Verify that a "Yes" OR "No" image will appear if the <h1> 
         // is "YES" OR "NO" respectively
         const component = shallow(<App  />);
-        let button = component.find('button')
-        button.simulate('click')
+        let button = component.find('button');
 
-        let img = component.find('img > alt')
-        let h1 = component.find('h1')
+        button.simulate('click');
+        component.update();
+
+        let img = component.find('img > alt');
+        let h1 = component.find('h1');
          
-        expect(img.text).toEqual(h1.text)
+        expect(img.text).toEqual(h1.text);
     });
 });
